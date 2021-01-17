481a11fe3e2ac03b69af1054462082d3a267b58c
Lena <helly.luchina@gmail.com>
2021-01-17 21:06:57 +0300

    cleared users.js file

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,15 +2,6 @@ const bcrypt = require('bcryptjs')
 const {sign} = require("jsonwebtoken");
 const Database = require('./database');
 
-class User {
-	constructor(id, name, email, password) {
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.password = password;
-	}
-}
-
 class UserAbstraction {
 	async emailIsTaken(email) {
 		const db = new Database();
@@ -88,40 +79,3 @@ exports.post = async (req, res) => {
 		}
 	}
 };
-
-
-// exports.post = async (req, res) => {
-// 	const db = new Database();
-// 	try {
-// 		console.log(req.body);
-// 		let {name, email, password, confirmedpassword} = req.body;
-// 		if (!name || !email || !password || !confirmedpassword)
-// 			res.status(400).json({msg: "not all data provided"});
-// 		else if (password != confirmedpassword)
-// 			res.status(400).json({msg: "passwords do not match"});
-// 		else {
-// 			const users = await db.query(`SELECT * FROM users WHERE email = ?`, email);
-// 			if (users.length > 0)
-// 				res.status(400).json({msg: "email is already registered"});
-// 			else
-// 			{
-// 				password = await bcrypt.hash(password, 10);
-// 				const idresults = await db.query("SELECT uuid() as id");
-// 				const id = idresults[0].id;
-// 				const newuser = new User(id, name, email, password);
-// 				await db.query("INSERT into users SET ?", [newuser]);
-// 				const jsontoken  = sign({}, process.env.JWTKEY, {
-// 					expiresIn: "1h"
-// 				});
-// 				res.status(200).json({jwt: jsontoken});
-// 			}
-// 		}
-// 	}
-// 	catch(err) {
-// 		console.log(err)
-// 		res.status(500).send(err)
-// 	}
-// 	finally {
-// 		await db.close();
-// 	}
-// };
